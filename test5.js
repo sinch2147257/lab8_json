08ff8320a4ec1c0ba2cdf497608fe43887ab45d1
Sinchana S <96084199+sinch2147257@users.noreply.github.com>
2022-02-07 01:21:56 +0530

    Delete test5.js

--- test5.js
+++
@@ -1,9 +0,0 @@
-var myapp=angular.module("myapp",[]);
-myapp.controller("myctrl",function($scope,$http)
-{
-    $http.get('url')
-    .success(function(response)
-    {
-        $scope.names=response.records;
-    });
-});
